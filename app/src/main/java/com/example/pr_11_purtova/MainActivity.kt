3138f3c7c07de38bdbe3b4251836deb7c535e941
PurpleAss <purtovajagg06@gmail.com>
2022-06-03 11:48:44 +0700

    start commit

---
+++ app/src/main/java/com/example/pr_11_purtova/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pr_11_purtova
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/pr_11_purtova/PR_11_Purtova_2.kt
@@ -0,0 +1,11 @@
+package com.example.pr_11_purtova
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PR_11_Purtova_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pr11_purtova2)
+    }
+}
